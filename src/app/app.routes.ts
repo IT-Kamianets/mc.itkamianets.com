c14a2b5be3275c8fe23ae8402d871715bd73a1c8
Keyd8n <kn1b22.demchenko@kpnu.edu.ua>
2026-02-18 12:14:35 +0200

    feat: видалив невикористані сторінки та компоненти

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { Public } from './layouts/public/public';
 import { Contacts } from './pages/contacts/contacts';
-import { Content } from './pages/content/content';
-import { Form } from './pages/form/form';
 import { Gallery } from './pages/gallery/gallery';
 import { Home } from './pages/home/home';
 import { List } from './pages/list/list';
 import { Profile } from './pages/profile/profile';
-import { Table } from './pages/table/table';
 
 export const routes: Routes = [
 	{
@@ -27,20 +24,8 @@ export const routes: Routes = [
 				component: Profile,
 			},
 			{
-				path: 'form',
-				component: Form,
-			},
-			{
-				path: 'table',
-				component: Table,
-			},
-			{
 				path: 'gallery',
 				component: Gallery,
-			},
-			{
-				path: 'content',
-				component: Content,
 			},
 			{
 				path: 'contacts',
